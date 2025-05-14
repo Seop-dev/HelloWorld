2318f1a60e0b29a957aec036ee42b7c78fae243a
Seop-dev <Devseop97@gmail.com>
2025-05-14 12:29:42 +0900

    Update JSExe.java

--- HelloWorld/src/com/yedam/JSExe.java
+++ HelloWorld/src/com/yedam/JSExe.java
@@ -2,11 +2,11 @@ package com.yedam;
 
 public class JSExe {
 	public static void main(String[] args) {
-		// 임의의 수를 생성 1 ~ 10 사이의 임의의 값 생성
-		// 0 <= x < 11 
+		// 임의의 수를 생성 1 ~ 100 사이의 임의의 값 생성
+		// 0 <= x < 101 
 		int sum = 0;
 		for (int i = 1; i <= 5; i++) {
-			int result = (int) (Math.random() * 10) + 1;
+			int result = (int) (Math.random() * 100) + 1;
 			sum += result;
 		}
 		System.out.println("결과: " + sum);
